00f4fcb1a6cd56bbed548f5af1bc29de8bdb8c59
Daniel Grijuela <danielgrijuela@gmail.com>
2014-12-29 10:07:12 +0100

    Remove dir variable to make it work anywhere

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,11 +3,8 @@
 # This script creates symlinks from the home directory to my
 # desired dotfiles which are in ~/dotfiles
 
-# Variables
-
-dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc gitconfig"     # list of files/folders to symlink in ~
+files="vimrc zshrc gitconfig bashrc bash_profile"     # list of files/folders to symlink in ~
 theme=dgrijuela.zsh-theme         # custom theme
 plugin=dgrijuela.plugin.zsh       # custom plugin
 
@@ -18,7 +15,7 @@ for file in $files; do
     echo "Moving any conflicting dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory"
-    ln -s $dir/$file ~/.$file
+    ln -s $file ~/.$file
 done
 
 echo "Cloning vundle inside vim to install vim plugins and then installing them"
@@ -40,6 +37,6 @@ echo "Cloning oh-my-zsh to make zsh even more awesome"
 git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Creating symlinks for my custom theme and plugin for oh-my-zsh"
-ln -s $dir/oh-my-zsh/custom/$theme ~/.oh-my-zsh/custom/$theme
+ln -s oh-my-zsh/custom/$theme ~/.oh-my-zsh/custom/$theme
 mkdir ~/.oh-my-zsh/custom/plugins/dgrijuela
-ln -s $dir/oh-my-zsh/custom/plugins/dgrijuela/$plugin ~/.oh-my-zsh/custom/plugins/dgrijuela/$plugin
+ln -s oh-my-zsh/custom/plugins/dgrijuela/$plugin ~/.oh-my-zsh/custom/plugins/dgrijuela/$plugin
